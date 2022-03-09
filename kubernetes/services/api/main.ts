cfa635c6cf83f5ac65bbc2a7428865625dd30666
Kyle Ritchi <kyle@debugged.nl>
2022-03-09 10:38:41 +0100

    add services for workshop

--- kubernetes/services/api/main.ts
+++ kubernetes/services/api/main.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({hello: "world", time: new Date().toISOString()})
 })
 
 app.listen(port, () => {
