53840930625f6614b067297d54f9c7e497e4d6a2
Kyle Ritchi <kyle@debugged.nl>
2022-03-09 11:51:58 +0100

    update frontend

--- kubernetes/services/api/main.ts
+++ kubernetes/services/api/main.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send({hello: "world", time: new Date().toISOString()})
+  res.send({title: "Kubernetes workshop from API", time: new Date().toISOString()})
 })
 
 app.listen(port, () => {
--- kubernetes/services/frontend/src/app/app.component.ts
+++ kubernetes/services/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kubernetes-workshop';
+  public title = 'Loading...';
+  public data: any;
+
+  constructor(private http: HttpClient) {
+    this.getConfig().subscribe({
+      next: response => {
+        if (response) {
+          this.title = `${response.title} app is running!`;
+          this.data = response;
+        }
+      },
+      error: (error) => {
+        this.title = `ERROR: ${error.message}. STATUS: ${error.status}`;
+        this.data = error;
+      }});
+  }
+
+  getConfig() {
+    return this.http.get<any>('http://localhost:9080/api');
+  }
 }
