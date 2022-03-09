81b3e4a93926e6fb7bb4e1fdd26e686e55426bdc
toonvanstrijp <toonvanstrijp@gmail.com>
2022-03-09 11:44:21 +0100

    use ingress gateway

--- kubernetes/services/api/main.ts
+++ kubernetes/services/api/main.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send({hello: "world", time: new Date().toISOString()})
 })
 
