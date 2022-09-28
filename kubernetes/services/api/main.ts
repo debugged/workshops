8b63ceb4ed67674464388f6c810839bdfa1fd88a
Toon van Strijp <toonvanstrijp@gmail.com>
2022-09-28 10:18:55 +0200

    update kubernetes course

--- kubernetes/services/api/main.ts
+++ kubernetes/services/api/main.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send({title: "Kubernetes workshop from API", time: new Date().toISOString()})
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening on port ${port}`)
+})
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.log('HTTP server closed');
+  })
 })
