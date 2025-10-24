deed65813373e8bd5fb08c0530c651d176a20790
Olivier Bouchoms <olivier@debugged.nl>
2025-10-24 14:34:50 +0200

    chore: build step 5 and 6

---
+++ docker/5. Multi stage/src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from '/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Hello Fontys!</h1>
+    </div>
+  )
+}
+
+export default App
